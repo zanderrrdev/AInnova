219229ba786849333b7a82d216b977d07d4fc93c
Zanderr'r <62427605+zanderrrdev@users.noreply.github.com>
2024-11-02 06:02:56 -1000

    Add methods setToken and setModel

--- src/ainnova/Main.php
+++ src/ainnova/Main.php
@@ -2,12 +2,14 @@
 
 namespace ainnova;
 
+use ainnova\utils\Logger;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 
 class Main extends PluginBase {
 
 	private static ?Main $instance = null;
+	private ?Client $api = null;
 
 	/**
 	 * Get Main class
@@ -20,5 +22,24 @@ class Main extends PluginBase {
 
 	public function onEnable(): void {
 		self::$instance = $this;
+		Logger::info('AInnova plugin loaded successfully');
+	}
+
+	/**
+	 * Set the API token
+	 *
+	 * @param string $token
+	 */
+	public function setToken(string $token): void {
+		$this->api->setToken($token);
+	}
+
+	/**
+	 * Set the AI model
+	 *
+	 * @param string $model
+	 */
+	public function setModel(string $model): void {
+		$this->api->setModel($model);
 	}
 }
